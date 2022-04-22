2ef1d50da37e59f2057a367f3233b6e51eca32b7
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2022-04-22 15:04:29 +0200

    events: add tests, fix scope fds updating

--- src/events.rs
+++ src/events.rs
@@ -4,6 +4,7 @@ use std::hash::Hash;
 use std::ops::Deref;
 use std::os::unix::io::RawFd;
 use std::ptr;
+
 use nix::sys::epoll::{epoll_create, epoll_ctl, epoll_wait, EpollEvent, EpollFlags, EpollOp};
 use strum::IntoEnumIterator;
 use strum_macros::EnumIter;
@@ -32,28 +33,34 @@ type EventCallback = fn(fd: RawFd) -> Result<(), Error>;
 type EventMap = HashMap<ReactorEvent, EventCallback>;
 
 struct ScopedReactor {
-    fds: HashMap<RawFd, EventMap>
+    fds: HashMap<RawFd, EventMap>,
 }
 
-struct EventHandler<'x> {
+struct EventHandler {
     callback: EventCallback,
-    source: Option<&'x ScopedReactor>
+    scope: Option<usize>,
 }
 
-type EventHandlerMap<'x> = HashMap<ReactorEvent, EventHandler<'x>>;
+type EventHandlerMap = HashMap<ReactorEvent, EventHandler>;
 
-struct Reactor<'x> {
+struct Reactor {
     epoll_fd: RawFd,
-    fds: HashMap<RawFd, EventHandlerMap<'x>>
+    fds: HashMap<RawFd, EventHandlerMap>,
 }
 
 #[derive(EnumIter, Debug, PartialEq, Eq, Hash, Copy, Clone)]
 enum ReactorEvent {
     ReadyRead,
-    ReadyWrite
+    ReadyWrite,
 }
 
 impl ScopedReactor {
+    fn new() -> Self {
+        Self {
+            fds: Default::default(),
+        }
+    }
+
     pub fn contains(&self, fd: &RawFd, event: &ReactorEvent) -> bool {
         if !self.fds.contains_key(fd) {
             return false;
@@ -98,33 +105,35 @@ trait MutableReactor {
     fn remove(&mut self, fd: RawFd, event: ReactorEvent) -> Result<(), Error>;
 }
 
-impl<'x> Reactor<'x> {
-    pub fn create() -> Reactor<'x> {
+impl Reactor {
+    pub fn create() -> Reactor {
         Reactor {
             epoll_fd: epoll_create().unwrap(),
-            fds: Default::default()
+            fds: Default::default(),
         }
     }
 
-    fn add_fd(&mut self, fd: RawFd, revent: ReactorEvent, handler: EventCallback, source: Option<&'x ScopedReactor>) -> Result<(), Error> {
-        if ! self.fds.contains_key(&fd) {
+    fn add_fd(&mut self, fd: RawFd, revent: ReactorEvent, handler: EventCallback, source: Option<&ScopedReactor>) -> Result<(), Error> {
+        if !self.fds.contains_key(&fd) {
             self.fds.insert(fd, EventHandlerMap::new());
         }
 
         let infos = self.fds.get_mut(&fd).unwrap();
         let event_handler = EventHandler {
             callback: handler,
-            source
+            scope: source.map(|s| { ptr::addr_of!(*s) as *const i32 as usize }),
         };
 
         {
-            let current = infos.get(&revent);
-            if current.is_some() {
-                let current_source = current.unwrap().source;
-                if !(current_source.is_some() && source.is_some() && ptr::eq(current_source.unwrap(), source.unwrap())) {
-                    return Err(Error::DuplicatedFdWithDifferentSource);
-                }
-                if !(current_source.is_none() && source.is_none()) {
+            let old_handler = infos.get(&revent);
+            if old_handler.is_some() {
+                let old_source = old_handler.unwrap().scope;
+
+                if old_source.is_some() && source.is_some() {
+                    if old_source.unwrap() != event_handler.scope.unwrap() {
+                        return Err(Error::DuplicatedFdWithDifferentSource);
+                    }
+                } else if !old_source.is_none() || !source.is_none() {
                     return Err(Error::DuplicatedFdWithDifferentSource);
                 }
             }
@@ -171,15 +180,22 @@ impl<'x> Reactor<'x> {
         }
     }
 
-    pub fn update_scope(&mut self, scope: &'x ScopedReactor) -> Result<(), Error> {
+    #[must_use]
+    pub fn update_scope(&mut self, scope: &ScopedReactor) -> Result<(), Error> {
+        let scope_ptr = ptr::addr_of!(*scope) as *const i32 as usize;
+
         let pending_removal = {
             let mut items: HashMap<RawFd, Vec<ReactorEvent>> = Default::default();
 
             for (fd, events_handler_map) in self.fds.iter() {
                 for (event, handler) in events_handler_map.iter() {
-                    if handler.source.is_some() && ptr::eq(handler.source.unwrap(), scope) {
+                    if handler.scope.is_none() {
+                        continue;
+                    }
+
+                    if handler.scope.unwrap() == scope_ptr {
                         if !scope.contains(fd, event) {
-                            if ! items.contains_key(fd) {
+                            if !items.contains_key(fd) {
                                 items.insert(*fd, Vec::new());
                             }
                             items.get_mut(fd).unwrap().push(*event);
@@ -206,29 +222,24 @@ impl<'x> Reactor<'x> {
 
         return Ok(());
     }
-    fn new_scope() -> ScopedReactor {
-        ScopedReactor {
-            fds: Default::default(),
-        }
-    }
 }
 
-impl<'x> MutableReactor for Reactor<'x> {
+impl MutableReactor for Reactor {
     fn add(&mut self, fd: RawFd, revent: ReactorEvent, handler: EventCallback) -> Result<(), Error> {
         self.add_fd(fd, revent, handler, None)
     }
 
     fn remove(&mut self, fd: RawFd, event: ReactorEvent) -> Result<(), Error> {
-        if ! self.fds.contains_key(&fd) { return Ok(()); }
+        if !self.fds.contains_key(&fd) { return Ok(()); }
 
         let info = self.fds.get_mut(&fd).unwrap();
+        info.remove(&event);
 
-        if info.len() > 1 {
+        if info.len() > 0 {
             // remove watching for single event
-            info.remove(&event);
             let mut epoll_event = Some(EpollEvent::new(ReactorEvent::epoll_flags(info.keys()), 0));
             epoll_ctl(self.epoll_fd, EpollOp::EpollCtlMod, fd, &mut epoll_event)?;
-        } else if info.len() == 1 {
+        } else {
             // delete whole fd
             epoll_ctl(self.epoll_fd, EpollOp::EpollCtlDel, fd, None)?;
         }
@@ -239,7 +250,7 @@ impl<'x> MutableReactor for Reactor<'x> {
 
 impl MutableReactor for ScopedReactor {
     fn add(&mut self, fd: RawFd, revent: ReactorEvent, handler: EventCallback) -> Result<(), Error> {
-        if ! self.fds.contains_key(&fd) {
+        if !self.fds.contains_key(&fd) {
             self.fds.insert(fd, EventMap::new());
         }
         self.fds.get_mut(&fd).unwrap().insert(revent, handler);
@@ -253,5 +264,64 @@ impl MutableReactor for ScopedReactor {
         }
 
         return Ok(());
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::os::unix::io::RawFd;
+
+    use crate::events::{Error, MutableReactor, Reactor, ReactorEvent, ScopedReactor};
+
+    fn assert_fd_listeners_count(reactor: &Reactor, fd: RawFd, count: usize) {
+        assert!(reactor.fds.contains_key(&fd));
+        assert_eq!(reactor.fds.get(&fd).unwrap().len(), count);
+    }
+
+    #[test]
+    fn reactor_crud() {
+        let mut r = Reactor::create();
+        r.add(1, ReactorEvent::ReadyRead, |fd| { Ok(()) });
+        assert_fd_listeners_count(&r, 1, 1);
+        r.add(1, ReactorEvent::ReadyWrite, |fd| { Ok(()) });
+        assert_fd_listeners_count(&r, 1, 2);
+        r.remove(1, ReactorEvent::ReadyRead);
+        assert_fd_listeners_count(&r, 1, 1);
+        r.remove(1, ReactorEvent::ReadyWrite);
+        assert_fd_listeners_count(&r, 1, 0);
+    }
+
+    #[test]
+    fn scopes() {
+        let global_fd = 1;
+        let scope_fd = 2;
+
+        let mut r = Reactor::create();
+        let cb = |fd| { Ok(()) };
+        r.add(global_fd, ReactorEvent::ReadyRead, cb);
+
+        let mut s = ScopedReactor::new();
+
+        s.add_output(global_fd, cb);
+        s.add_output(2, cb);
+
+        r.update_scope(&s).unwrap();
+        assert_fd_listeners_count(&r, global_fd, 2);
+        assert_fd_listeners_count(&r, scope_fd, 1);
+
+        // remove existing fd and add a new one
+        s.remove(2, ReactorEvent::ReadyWrite);
+        r.update_scope(&s).unwrap();
+
+        assert_fd_listeners_count(&r, scope_fd, 0);
+
+        // add duplicated global fd
+        s.add_input(1, cb).unwrap();
+        assert!(matches!(r.update_scope(&s), Err(Error::DuplicatedFdWithDifferentSource)));
+
+        // add dublicated fd in another scope
+        let ns = ScopedReactor::new();
+        s.add_input(1, cb).unwrap();
+        assert!(matches!(r.update_scope(&s), Err(Error::DuplicatedFdWithDifferentSource)));
     }
 }
